7c6f090448acfc7a0e9b4cd9a2938c38d7a18693
Leif Walsh <leif@twosigma.com>
2021-03-24 02:49:48 +0000

    update copyright years

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Two Sigma Open Source, LLC
+ * Copyright 2020-2021 Two Sigma Open Source, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
