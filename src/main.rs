323f03c2b1d8024878fab74da7259cad6ed3ac4a
Ben Linsay <blinsay@gmail.com>
2021-06-29 00:26:48 -0400

    Use fixed pool of threads to handle incoming requests
    Uses a fixed pool of threads to handle incoming requests. Threads pass a
    UnixStream over a channel to each other. If no worker is available
    inside the libc timeout for nscd, forces an exit.
    
    Adds a WorkGroup to coordinate the pool of threads which allows blocking
    until any of the worker threads panic or exit normally.
    
    Closes #24

--- src/main.rs
+++ src/main.rs
@@ -49,17 +49,132 @@ use std::io::ErrorKind;
 use std::os::unix::fs::PermissionsExt;
 use std::os::unix::net::{UnixListener, UnixStream};
 use std::path::Path;
-use std::thread;
+use std::time::Duration;
 
 use anyhow::{Context, Result};
+use crossbeam_channel as channel;
 use slog::{debug, error, o, Drain};
 
 mod ffi;
 mod handlers;
 mod protocol;
+mod work_group;
 
-/// Handle a new socket connection, reading the request and sending the response.
-fn handle_stream(log: slog::Logger, mut stream: UnixStream) {
+use work_group::WorkGroup;
+
+const SOCKET_PATH: &str = "/var/run/nscd/socket";
+const N_WORKERS: usize = 256;
+const HANDOFF_TIMEOUT: Duration = Duration::from_secs(3);
+
+fn main() -> Result<()> {
+    ffi::disable_internal_nscd();
+
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    let logger = slog::Logger::root(drain, slog::o!());
+
+    let mut wg = WorkGroup::new();
+    let tx = spawn_workers(&mut wg, &logger, N_WORKERS);
+
+    let path = Path::new(SOCKET_PATH);
+    std::fs::create_dir_all(path.parent().expect("socket path has no parent"))?;
+    std::fs::remove_file(path).ok();
+    let listener = UnixListener::bind(path).context("could not bind to socket")?;
+    std::fs::set_permissions(path, std::fs::Permissions::from_mode(0o777))?;
+    spawn_acceptor(&mut wg, &logger, listener, tx);
+
+    let (result, handles) = wg.run();
+    if let Err(e) = result {
+        // if a thread unwound with a panic, just start panicing here. the nss
+        // modules in this process space are probably very sad, and we'll
+        // stop serving requests abruptly.
+        std::panic::resume_unwind(e);
+    } else {
+        // something else happened that made a process exit, so try to exit
+        // gracefully.
+        slog::info!(logger, "shutting down");
+        for handle in handles {
+            let _ = handle.join();
+        }
+        Ok(())
+    }
+}
+
+fn spawn_acceptor(
+    wg: &mut WorkGroup,
+    log: &slog::Logger,
+    listener: UnixListener,
+    tx: channel::Sender<UnixStream>,
+) {
+    let log = log.new(o!("thread" => "accept"));
+
+    wg.add(move |ctx| {
+        for stream in listener.incoming() {
+            if ctx.is_shutdown() {
+                break
+            }
+
+            match stream {
+                // if something goes wrong and it's multiple seconds until we
+                // get a response, kill the process.
+                //
+                // the timeout here is set such that nss will fall back to system
+                // libc before this timeout is hit - clients will already be
+                // giving up and going elsewhere so crashing the process should
+                // not make a bad situation worse.
+                Ok(stream) => match tx.send_timeout(stream, HANDOFF_TIMEOUT) {
+                    Err(channel::SendTimeoutError::Timeout(_)) => {
+                        error!(log, "timed out waiting for an available worker");
+                        break;
+                    }
+                    Err(channel::SendTimeoutError::Disconnected(_)) => {
+                        error!(log, "worker channel is disconnected");
+                        break;
+                    }
+                    Ok(()) => { /*ok!*/ }
+                },
+                Err(err) => {
+                    error!(log, "error accepting connection"; "err" => %err);
+                    break;
+                }
+            }
+        }
+
+        // at the end of the listener loop, drop tx so that any working threads
+        // still waiting for a connection have a chance to finish.
+        //
+        // this is unnecessary but explicit
+        std::mem::drop(tx);
+    });
+}
+
+fn spawn_workers(
+    wg: &mut WorkGroup,
+    log: &slog::Logger,
+    n_workers: usize,
+) -> channel::Sender<UnixStream> {
+    let (tx, rx) = channel::bounded(0);
+
+    for worker_id in 0..n_workers {
+        let rx = rx.clone();
+        let log = log.new(o!("thread" => format!("worker_{}", worker_id)));
+
+        // ctx is ignored - the acceptor thread will close the rx channel if
+        // the wg is shutdown and it's time to exit.
+        wg.add(move |_ctx| loop {
+            match rx.recv() {
+                Ok(stream) => handle_stream(&log, stream),
+                Err(channel::RecvError) => break,
+            }
+        });
+    }
+
+    tx
+}
+
+fn handle_stream(log: &slog::Logger, mut stream: UnixStream) {
     debug!(log, "accepted connection"; "stream" => ?stream);
     let mut buf = [0; 4096];
     let size_read = match stream.read(&mut buf) {
@@ -99,37 +214,4 @@ fn handle_stream(log: slog::Logger, mut stream: UnixStream) {
     if let Err(e) = stream.shutdown(std::net::Shutdown::Both) {
         debug!(log, "shutting down stream"; "err" => %e);
     }
-}
-
-const SOCKET_PATH: &str = "/var/run/nscd/socket";
-
-fn main() -> Result<()> {
-    ffi::disable_internal_nscd();
-
-    let decorator = slog_term::TermDecorator::new().build();
-    let drain = slog_term::FullFormat::new(decorator).build().fuse();
-    let drain = slog_async::Async::new(drain).build().fuse();
-
-    let logger = slog::Logger::root(drain, slog::o!());
-
-    let path = Path::new(SOCKET_PATH);
-    std::fs::create_dir_all(path.parent().expect("socket path has no parent"))?;
-    std::fs::remove_file(path).ok();
-    let listener = UnixListener::bind(path).context("could not bind to socket")?;
-    std::fs::set_permissions(path, std::fs::Permissions::from_mode(0o777))?;
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                let thread_logger = logger.clone();
-                thread::spawn(move || handle_stream(thread_logger, stream));
-            }
-            Err(err) => {
-                error!(logger, "error accepting connection"; "err" => %err);
-                break;
-            }
-        }
-    }
-
-    Ok(())
 }
