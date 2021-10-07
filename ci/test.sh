f4f44004c16d98aea8623bdb9f93333b0bbef2e7
Geoffrey Thomas <geofft@twosigma.com>
2021-10-07 17:14:21 -0400

    Add failing test for nix-rust/nix#1541

--- ci/test.sh
+++ ci/test.sh
@@ -20,6 +20,7 @@ debian/rules vendor
 dpkg-buildpackage --no-sign
 gcc -fPIC -shared -o ci/libnss_whatami.so.2 ci/libnss_whatami.c
 sudo cp ci/libnss_whatami.so.2 /lib
-sudo sed -i 's/passwd:/& whatami/' /etc/nsswitch.conf
+sudo sed -i 's/\(passwd\|group\):/& whatami/' /etc/nsswitch.conf
 sudo dpkg -i ../nsncd*.deb
 getent passwd whatami | grep nsncd
+getent initgroups am_i_nsncd | grep '100001.*100020'
