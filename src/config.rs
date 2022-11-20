cd4e7713d15c95065ab05bcf5e49fab6d22e79c9
Leif Walsh <leif.walsh@gmail.com>
2022-11-20 20:39:02 +0000

    change ignore interface to match nsswitch.conf

--- src/config.rs
+++ src/config.rs
@@ -14,10 +14,10 @@
  * limitations under the License.
  */
 
-//! Command line flags (to control which requests we respond to).
+//! Configuration for nsncd.
 
-use std::env;
 use std::time::Duration;
+use std::{collections::BTreeMap, env};
 
 use anyhow::{Context, Result};
 
@@ -27,89 +27,83 @@ use super::protocol::RequestType;
 pub struct Config {
     pub worker_count: usize,
     pub handoff_timeout: Duration,
-    ignore_getpwbyname: bool,
-    ignore_getpwbyuid: bool,
-    ignore_getgrbyname: bool,
-    ignore_getgrbygid: bool,
-    ignore_initgroups: bool,
-    ignore_gethostbyaddr: bool,
-    ignore_gethostbyaddrv6: bool,
-    ignore_gethostbyname: bool,
-    ignore_gethostbynamev6: bool,
-    ignore_shutdown: bool,
-    ignore_getstat: bool,
-    ignore_invalidate: bool,
-    ignore_getfdpw: bool,
-    ignore_getfdgr: bool,
-    ignore_getfdhst: bool,
-    ignore_getai: bool,
-    ignore_getservbyname: bool,
-    ignore_getservbyport: bool,
-    ignore_getfdserv: bool,
-    ignore_getfdnetgr: bool,
-    ignore_getnetgrent: bool,
-    ignore_innetgr: bool,
+    pub ignored_request_types: [bool; 32],
 }
+
+/// Mapping from nsswitch.conf "database" name to the request types related to
+/// that database.
+const OPS_BY_DATABASE: &[(&str, &[RequestType])] = &[
+    (
+        "group",
+        &[RequestType::GETGRBYNAME, RequestType::GETGRBYGID],
+    ),
+    (
+        "hosts",
+        &[
+            RequestType::GETHOSTBYADDR,
+            RequestType::GETHOSTBYADDRv6,
+            RequestType::GETHOSTBYNAME,
+            RequestType::GETHOSTBYNAMEv6,
+            RequestType::GETAI,
+        ],
+    ),
+    ("initgroups", &[RequestType::INITGROUPS]),
+    (
+        "netgroup",
+        &[RequestType::GETNETGRENT, RequestType::INNETGR],
+    ),
+    (
+        "passwd",
+        &[RequestType::GETPWBYNAME, RequestType::GETPWBYUID],
+    ),
+    (
+        "services",
+        &[RequestType::GETSERVBYNAME, RequestType::GETSERVBYPORT],
+    ),
+];
 
 impl Config {
     pub fn from_env() -> Result<Self> {
+        let ops_map = {
+            let mut ops_map = BTreeMap::new();
+            for (op_group, types) in OPS_BY_DATABASE.iter() {
+                ops_map.insert(op_group.to_uppercase().to_string(), *types);
+            }
+            ops_map
+        };
+
+        let mut ignored_request_types = [false; 32];
+
+        for (key, value) in env::vars() {
+            let op_group = match key.strip_prefix("NSNCD_IGNORE_") {
+                Some(name) => name,
+                None => continue,
+            };
+            let types = ops_map.get(op_group).ok_or_else(|| {
+                let groups = ops_map.keys().cloned().collect::<Vec<_>>().join(", ");
+                anyhow::format_err!("Unknown group '{}'. Choose from: {}", op_group, groups)
+            })?;
+            let value = value
+                .parse()
+                .with_context(|| format!("parsing bool from {}", value))?;
+            if value {
+                for ty in types.iter() {
+                    ignored_request_types[*ty as usize] = true;
+                }
+            }
+        }
+
         Ok(Self {
             worker_count: env_positive_usize("NSNCD_WORKER_COUNT", 8)?,
             handoff_timeout: Duration::from_secs(
                 env_positive_usize("NSNCD_HANDOFF_TIMEOUT", 3)? as u64
             ),
-            ignore_getpwbyname: env_bool("NSNCD_IGNORE_GETPWBYNAME")?,
-            ignore_getpwbyuid: env_bool("NSNCD_IGNORE_GETPWBYUID")?,
-            ignore_getgrbyname: env_bool("NSNCD_IGNORE_GETGRBYNAME")?,
-            ignore_getgrbygid: env_bool("NSNCD_IGNORE_GETGRBYGID")?,
-            ignore_initgroups: env_bool("NSNCD_IGNORE_INITGROUPS")?,
-            ignore_gethostbyaddr: env_bool("NSNCD_IGNORE_GETHOSTBYADDR")?,
-            ignore_gethostbyaddrv6: env_bool("NSNCD_IGNORE_GETHOSTBYADDRV6")?,
-            ignore_gethostbyname: env_bool("NSNCD_IGNORE_GETHOSTBYNAME")?,
-            ignore_gethostbynamev6: env_bool("NSNCD_IGNORE_GETHOSTBYNAMEV6")?,
-            ignore_shutdown: env_bool("NSNCD_IGNORE_SHUTDOWN")?,
-            ignore_getstat: env_bool("NSNCD_IGNORE_GETSTAT")?,
-            ignore_invalidate: env_bool("NSNCD_IGNORE_INVALIDATE")?,
-            ignore_getfdpw: env_bool("NSNCD_IGNORE_GETFDPW")?,
-            ignore_getfdgr: env_bool("NSNCD_IGNORE_GETFDGR")?,
-            ignore_getfdhst: env_bool("NSNCD_IGNORE_GETFDHST")?,
-            ignore_getai: env_bool("NSNCD_IGNORE_GETAI")?,
-            ignore_getservbyname: env_bool("NSNCD_IGNORE_GETSERVBYNAME")?,
-            ignore_getservbyport: env_bool("NSNCD_IGNORE_GETSERVBYPORT")?,
-            ignore_getfdserv: env_bool("NSNCD_IGNORE_GETFDSERV")?,
-            ignore_getfdnetgr: env_bool("NSNCD_IGNORE_GETFDNETGR")?,
-            ignore_getnetgrent: env_bool("NSNCD_IGNORE_GETNETGRENT")?,
-            ignore_innetgr: env_bool("NSNCD_IGNORE_INNETGR")?,
+            ignored_request_types,
         })
     }
 
     pub fn should_ignore(&self, ty: &RequestType) -> bool {
-        match ty {
-            RequestType::GETPWBYNAME => self.ignore_getpwbyname,
-            RequestType::GETPWBYUID => self.ignore_getpwbyuid,
-            RequestType::GETGRBYNAME => self.ignore_getgrbyname,
-            RequestType::GETGRBYGID => self.ignore_getgrbygid,
-            RequestType::GETHOSTBYNAME => self.ignore_gethostbyname,
-            RequestType::GETHOSTBYNAMEv6 => self.ignore_gethostbynamev6,
-            RequestType::GETHOSTBYADDR => self.ignore_gethostbyaddr,
-            RequestType::GETHOSTBYADDRv6 => self.ignore_gethostbyaddrv6,
-            RequestType::SHUTDOWN => self.ignore_shutdown,
-            RequestType::GETSTAT => self.ignore_getstat,
-            RequestType::INVALIDATE => self.ignore_invalidate,
-            RequestType::GETFDPW => self.ignore_getfdpw,
-            RequestType::GETFDGR => self.ignore_getfdgr,
-            RequestType::GETFDHST => self.ignore_getfdhst,
-            RequestType::GETAI => self.ignore_getai,
-            RequestType::INITGROUPS => self.ignore_initgroups,
-            RequestType::GETSERVBYNAME => self.ignore_getservbyname,
-            RequestType::GETSERVBYPORT => self.ignore_getservbyport,
-            RequestType::GETFDSERV => self.ignore_getfdserv,
-            RequestType::GETNETGRENT => self.ignore_getnetgrent,
-            RequestType::INNETGR => self.ignore_innetgr,
-            RequestType::GETFDNETGR => self.ignore_getfdnetgr,
-            // This can't happen
-            RequestType::LASTREQ => false,
-        }
+        self.ignored_request_types[*ty as usize]
     }
 }
 
@@ -118,33 +112,12 @@ impl Default for Config {
         Self {
             worker_count: 8,
             handoff_timeout: Duration::from_secs(3),
-            ignore_getpwbyname: Default::default(),
-            ignore_getpwbyuid: Default::default(),
-            ignore_getgrbyname: Default::default(),
-            ignore_getgrbygid: Default::default(),
-            ignore_initgroups: Default::default(),
-            ignore_gethostbyaddr: Default::default(),
-            ignore_gethostbyaddrv6: Default::default(),
-            ignore_gethostbyname: Default::default(),
-            ignore_gethostbynamev6: Default::default(),
-            ignore_shutdown: Default::default(),
-            ignore_getstat: Default::default(),
-            ignore_invalidate: Default::default(),
-            ignore_getfdpw: Default::default(),
-            ignore_getfdgr: Default::default(),
-            ignore_getfdhst: Default::default(),
-            ignore_getai: Default::default(),
-            ignore_getservbyname: Default::default(),
-            ignore_getservbyport: Default::default(),
-            ignore_getfdserv: Default::default(),
-            ignore_getfdnetgr: Default::default(),
-            ignore_getnetgrent: Default::default(),
-            ignore_innetgr: Default::default(),
+            ignored_request_types: [false; 32],
         }
     }
 }
 
-fn env_positive_usize(var: &'static str, default: usize) -> Result<usize> {
+fn env_positive_usize(var: &str, default: usize) -> Result<usize> {
     let s = match env::var(var) {
         Ok(s) => s,
         Err(_) => return Ok(default),
@@ -159,15 +132,6 @@ fn env_positive_usize(var: &'static str, default: usize) -> Result<usize> {
     }
 }
 
-fn env_bool(var: &'static str) -> Result<bool> {
-    if let Ok(v) = env::var(var) {
-        Ok(v.parse()
-            .with_context(|| format!("parsing bool from {}", v))?)
-    } else {
-        Ok(false)
-    }
-}
-
 #[cfg(test)]
 mod test {
     use std::time::Duration;
@@ -175,14 +139,15 @@ mod test {
     use temp_env::{with_var, with_var_unset, with_vars};
 
     use super::Config;
+    use super::RequestType;
 
     #[test]
     fn test_defaults() {
         let config = Config::default();
         assert_eq!(config.worker_count, 8);
         assert_eq!(config.handoff_timeout, Duration::from_secs(3));
-        assert!(!config.ignore_getpwbyname);
-        assert!(!config.ignore_getpwbyuid);
+        assert!(!config.should_ignore(&RequestType::GETPWBYNAME));
+        assert!(!config.should_ignore(&RequestType::GETPWBYUID));
     }
 
     #[test]
@@ -254,19 +219,21 @@ mod test {
     fn test_ignore_vars() {
         with_var_unset("NSNCD_IGNORE_INITGROUPS", || {
             let config = Config::from_env().unwrap();
-            assert!(!config.ignore_getpwbyname);
-            assert!(!config.ignore_initgroups);
+            assert!(!config.should_ignore(&RequestType::GETPWBYNAME));
+            assert!(!config.should_ignore(&RequestType::INITGROUPS));
         });
         with_var("NSNCD_IGNORE_INITGROUPS", Some("true"), || {
             let config = Config::from_env().unwrap();
-            assert!(!config.ignore_getpwbyname);
-            assert!(config.ignore_initgroups);
+            assert!(!config.should_ignore(&RequestType::GETPWBYNAME));
+            assert!(config.should_ignore(&RequestType::INITGROUPS));
         });
         with_var("NSNCD_IGNORE_INITGROUPS", Some("false"), || {
             let config = Config::from_env().unwrap();
-            assert!(!config.ignore_getpwbyname);
-            assert!(!config.ignore_initgroups);
+            assert!(!config.should_ignore(&RequestType::GETPWBYNAME));
+            assert!(!config.should_ignore(&RequestType::INITGROUPS));
         });
+
+        // Invalid values
         with_var("NSNCD_IGNORE_INITGROUPS", Some("yes"), || {
             assert!(Config::from_env().is_err());
         });
@@ -288,44 +255,64 @@ mod test {
         with_var("NSNCD_IGNORE_INITGROUPS", Some(""), || {
             assert!(Config::from_env().is_err());
         });
+
+        // Invalid keys
+        with_var("NSNCD_IGNORE_initgroups", Some("true"), || {
+            assert!(Config::from_env().is_err());
+        });
+        with_var("NSNCD_IGNORE_ZZZNOTAGROUP", Some("true"), || {
+            assert!(Config::from_env().is_err());
+        });
+
+        // Some combinations
         with_vars(
             vec![
-                ("NSNCD_IGNORE_GETPWBYNAME", Some("false")),
+                ("NSNCD_IGNORE_PASSWD", Some("false")),
                 ("NSNCD_IGNORE_INITGROUPS", Some("true")),
             ],
             || {
                 let config = Config::from_env().unwrap();
-                assert!(!config.ignore_getpwbyname);
-                assert!(config.ignore_initgroups);
+                assert!(!config.should_ignore(&RequestType::GETPWBYNAME));
+                assert!(config.should_ignore(&RequestType::INITGROUPS));
             },
         );
         with_vars(
             vec![
-                ("NSNCD_IGNORE_GETPWBYNAME", Some("true")),
+                ("NSNCD_IGNORE_PASSWD", Some("true")),
                 ("NSNCD_IGNORE_INITGROUPS", Some("false")),
             ],
             || {
                 let config = Config::from_env().unwrap();
-                assert!(config.ignore_getpwbyname);
-                assert!(!config.ignore_initgroups);
+                assert!(config.should_ignore(&RequestType::GETPWBYNAME));
+                assert!(!config.should_ignore(&RequestType::INITGROUPS));
             },
         );
         with_vars(
             vec![
-                ("NSNCD_IGNORE_GETPWBYNAME", Some("true")),
+                ("NSNCD_IGNORE_PASSWD", Some("true")),
                 ("NSNCD_IGNORE_INITGROUPS", Some("true")),
             ],
             || {
                 let config = Config::from_env().unwrap();
-                assert!(config.ignore_getpwbyname);
-                assert!(!config.ignore_getpwbyuid);
-                assert!(config.ignore_initgroups);
+                assert!(config.should_ignore(&RequestType::GETPWBYNAME));
+                assert!(config.should_ignore(&RequestType::GETPWBYUID));
+                assert!(!config.should_ignore(&RequestType::GETGRBYGID));
+                assert!(config.should_ignore(&RequestType::INITGROUPS));
             },
         );
         with_vars(
             vec![
-                ("NSNCD_IGNORE_GETPWBYNAME", Some("1")),
+                ("NSNCD_IGNORE_PASSWD", Some("1")),
                 ("NSNCD_IGNORE_INITGROUPS", Some("true")),
+            ],
+            || {
+                assert!(Config::from_env().is_err());
+            },
+        );
+        with_vars(
+            vec![
+                ("NSNCD_IGNORE_PASSWD", Some("true")),
+                ("NSNCD_IGNORE_ZZZNOTAGROUP", Some("true")),
             ],
             || {
                 assert!(Config::from_env().is_err());
