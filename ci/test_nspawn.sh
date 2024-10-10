f09c0515fcbe0e1018a6f5ce4ca3a6d0fe76025f
James Morris <morisja@gmail.com>
2024-10-10 15:19:10 -0400

    Update enhanced CI tests

--- ci/test_nspawn.sh
+++ ci/test_nspawn.sh
@@ -3,7 +3,6 @@
 
 sudo dpkg -i nsncd*.deb
 
-sudo useradd nsncdtest 
 
 
 sudo debootstrap --variant=minbase stable /stable-chroot http://deb.debian.org/debian/
@@ -11,7 +10,18 @@ sdns="sudo systemd-nspawn -q --bind-ro /var/run/nscd/socket:/var/run/nscd/socket
 
 rc=0
 
+sudo useradd nsncdtest 
+echo -e "foo1\t65000/tcp" | sudo tee -a /etc/services
+echo -e "foo1\t65000/udp" | sudo tee -a /etc/services
+
 ${sdns} getent passwd nsncdtest || rc=1
 ${sdns} getent group nsncdtest || rc=1
+for i in $(seq 1 20); do
+	${sdns} getent services 65000 || rc=1
+	${sdns} getent services 65000/tcp || rc=1
+	${sdns} getent services 65000/udp || rc=1
+	${sdns} getent services foo1/tcp || rc=1
+	${sdns} getent services foo1/udp || rc=1
+done
 
 exit ${rc}
